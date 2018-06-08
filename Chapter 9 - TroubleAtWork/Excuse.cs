fc3a69f9206930f36dcbfcffab69c6a14bf47f97
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-06-08 15:59:11 +0200

    first commit

---
+++ Chapter 9 - TroubleAtWork/Excuse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_9___TroubleAtWork
+{
+    class Excuse
+    {
+        public String Description { get; private set; }
+        public String Results { get; private set; }
+        public DateTime LastUsed { get; private set; }
+        public String ExcusePath { get; private set; }
+
+        public void OpenFile(string name)
+        {
+
+        }
+
+        public void Save(string name)
+        {
+
+        }
+    }
+}
